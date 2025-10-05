e5903d8d0f3b835def5225df1bc01a51e43a66a3
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2025-10-05 12:04:59 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Azure.AI.OpenAI;
+using Azure.Identity;
+using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+using OpenAI;
+
+AIAgent agent = new AzureOpenAIClient(
+    new Uri("https://myaiserviceluiscoco.openai.azure.com/"),
+    new AzureCliCredential())
+        .GetChatClient("gpt-4o-mini")
+        .CreateAIAgent(instructions: "You are good at telling jokes.", name: "Joker");
+
+Console.WriteLine(await agent.RunAsync("Tell me a joke about a pirate."));
